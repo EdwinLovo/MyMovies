4de3b45c4a9189794982e9dfc8e1e4db182c1f47
EdwinLovo <00120617@uca.edu.sv>
2019-06-12 15:10:37 -0600

    connection bug fixed

--- app/src/main/java/com/pdm/mymovies/Fragments/HomeFragment.kt
+++ app/src/main/java/com/pdm/mymovies/Fragments/HomeFragment.kt
@@ -1,12 +1,16 @@
 package com.pdm.mymovies.Fragments
 
 
+import android.content.Context
 import android.content.res.Configuration
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
@@ -41,10 +45,14 @@ class HomeFragment : Fragment() {
         var adapter = object : MovieAdapter(view.context){
             override fun setClickListenerToMovie(holder: MovieViewHolder, movieName: String) {
                 holder.movieLinearLayout.setOnClickListener {
-                    movieViewModel.retrieveDetailMovies(movieName)
-                    if(orientation()){
-                        val nextAction = HomeFragmentDirections.nextAction()
-                        Navigation.findNavController(it).navigate(nextAction)
+                    if (isConnected(view.context)){
+                        movieViewModel.retrieveDetailMovies(movieName)
+                        if(orientation()){
+                            val nextAction = HomeFragmentDirections.nextAction()
+                            Navigation.findNavController(it).navigate(nextAction)
+                        }
+                    } else {
+                        Toast.makeText(view.context,"No internet connection", Toast.LENGTH_LONG).show()
                     }
                 }
             }
@@ -61,12 +69,22 @@ class HomeFragment : Fragment() {
         }*/
 
         view.search.setOnClickListener {
-            movieViewModel.retrieveMovies(view.title.text.toString())
+            if (isConnected(it.context)){
+                movieViewModel.retrieveMovies(view.title.text.toString())
+            } else {
+                Toast.makeText(it.context,"No internet connection", Toast.LENGTH_LONG).show()
+            }
         }
 
     }
 
     fun orientation():Boolean=resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
 
+    fun isConnected(context: Context):Boolean{
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork:NetworkInfo? = connectivityManager.activeNetworkInfo
+        val connected:Boolean = activeNetwork?.isConnected == true
+        return connected
+    }
 
 }
